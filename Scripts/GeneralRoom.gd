2bb8a1c0784b02f1d463c4d81f1e0725221523a1
Jkaos725 <jacobbutterhorn@gmail.com>
2026-03-21 09:48:03 -0500

    Using Grog

--- Scripts/GeneralRoom.gd
+++ Scripts/GeneralRoom.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
+var currentQuestion = "What is the derivative of sin(x)x^2"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	var timer = get_tree().get_first_node_in_group("GlobalTimer")
+	timer.timeout.connect(on_timer_timeout)
+	for child in get_children():
+		if ("currentQuestion" in child):
+			child.currentQuestion = currentQuestion
+			if ("text" in child):
+				child.text = currentQuestion
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func on_timer_timeout():
+	Global.globalTime -= 1
