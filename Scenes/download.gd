a25587a7212d466f8765513e25882866db98d271
Jkaos725 <jacobbutterhorn@gmail.com>
2026-03-21 00:13:14 -0500

    Update download.gd

--- Scenes/download.gd
+++ Scenes/download.gd
@@ -13,9 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Multiple_Choice.tscn")
-	pass # Replace with function body.
 
 
 func _on_door_left_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Questionhints.tscn")
-	pass # Replace with function body.
