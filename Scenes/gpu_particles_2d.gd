48b8827cd79cd97495a42543f6b5f77c1d054421
henry1310 <ng.quoc1310@gmail.com>
2026-03-21 15:26:07 -0500

    Particles

---
+++ Scenes/gpu_particles_2d.gd
@@ -0,0 +1,13 @@
+extends GPUParticles2D
+
+
+func _on_hint_1_pressed() -> void:
+	emitting = true# Replace with function body.
+
+
+func _on_hint_2_pressed() -> void:
+	emitting = true # Replace with function body.
+
+
+func _on_hint_3_pressed() -> void:
+	emitting = true # Replace with function body.
